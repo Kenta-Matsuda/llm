ed01a04e2647e0b44a15e6da36a65c9d26106e32
Matsuda Kenta <kenta.matsuda1125@gmail.com>
2024-03-27 23:40:05 +0900

    1.1 文書分類

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ text_classification_pipeline = pipeline(
 # text = "世界には言葉が出ないほどひどい音楽がある。"
 # text = "段ボールが破損していて商品も破損していました。"
 # text = "色が可愛くてまたリピートしたいと思います。"
-text = "色は可愛いですが、コスパが悪いのでリピートはしません。"
+# text = "色は可愛いですが、コスパが悪いのでリピートはしません。"
+text = "高評価だったので購入しましたが、私の肌には合いませんでした。"
 # textの極性を予測
 print(text_classification_pipeline(text)[0])
 
